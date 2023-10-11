e36b777316d180de93d26e5e6a2ddf51c1eb2929
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-11 13:26:55 -0300

    Add files via upload

---
+++ 2.2.1-hello_world/ex2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+double square(double x){
+    return x * x;
+}
+
+void print_square(double x){
+    cout << "The square of " << x << " is " << square(x) << "/n";
+}
+
+int main (){
+    print_square(4);
+}
