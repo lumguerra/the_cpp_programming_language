a45ffc2b99e01614ab92c1e332e952320b3deadb
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-12 18:17:41 -0300

    up

---
+++ 2.4-modularity/ex1.cpp
@@ -0,0 +1,11 @@
+double sqrt(double); // the square root function takes a double and returns a double
+class Vector {
+public:
+Vector(int s);
+double& operator[](int i);
+int size();
+private:
+double∗ elem; // elem points to an array of sz doubles
+int sz;
+};
+The key p
