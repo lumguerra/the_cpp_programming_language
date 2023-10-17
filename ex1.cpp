f12cb0fcf2a525ab83cf60628ee597008a83b85d
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-17 15:48:35 -0300

    up

---
+++ ex1.cpp
@@ -0,0 +1,18 @@
+namespace My_code {
+class complex { /* ... */ };
+complex sqr t(complex);
+// ...
+int main();
+}
+int My_code::main()
+{
+complex z {1,2};
+auto z2 = sqrt(z);
+std::cout << '{' << z2.real() << ',' << z2.imag() << "}\n";
+// ...
+};
+int main()
+{
+return My_code::main();
+}
+By puttin
