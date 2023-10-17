aa06cb5415d26f7f2e9aeaaa8444930d9520a559
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-17 17:24:05 -0300

    up

--- ex1.cpp
+++ ex1.cpp
@@ -1,17 +1,16 @@
-namespace My_code {
-class complex { /* ... */ };
-complex sqr t(complex);
-// ...
-int main();
-}
-int My_code::main()
-{
-complex z {1,2};
-auto z2 = sqrt(z);
-std::cout << '{' << z2.real() << ',' << z2.imag() << "}\n";
-// ...
+class complex {
+double re, im; // representation: two doubles
+public:
+complex(double r, double i) :re{r}, im{i} {} // construct complex from two scalars
+complex(double r) :re{r}, im{0} {} // construct complex from one scalar
+complex() :re{0}, im{0} {} // default complex: {0,0}
+double real() const { return re; }
+void real(double d) { re=d; }
+double imag() const { return im; }
+void imag(double d) { im=d; }
+complex& operator+=(complex z) { re+=z.re , im+=z.im; return ∗this; } // add to re and im
+// and return the result
+complex& operator−=(complex z) { re−=z.re , im−=z.im; return ∗this; }
+complex& operator∗=(complex); // defined out-of-class somewhere
+complex& operator/=(complex); // defined out-of-class somewhere
 };
-int main()
-{
-return My_code::main();
-}
